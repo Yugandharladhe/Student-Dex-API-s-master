d746e45f6786bb8cba80c58d62e3dba6d5003877
Yugandharladhe <yugandharladhe74@gmail.com>
2023-01-10 13:06:52 +0530

    tested

--- routers/forgotpassword.js
+++ routers/forgotpassword.js
@@ -2,12 +2,17 @@ const express=require("express");
 const Student = require("../modelsdb/schemastudent");
 const app = express();
 const router = new express.Router();
+const bcrypt=require("bcryptjs");
 
 router.post("/forgotpassword",async(req,res)=>{
     const stud=await Student.findOne({RollNo:req.body.RollNo});
     if(stud)
     {
-        const upd=await Student.updateOne({RollNo:req.body.RollNo},{$set:{Password:req.body.Password}});
+        //$2a$12$6ZSJ3fOlGZEF1bxbD2fBzutns38S7VZ2opiGiANc0UJCJHFx75GI2
+        //$2a$12$6ZSJ3fOlGZEF1bxbD2fBzutns38S7VZ2opiGiANc0UJCJHFx75GI2
+        const pass=await bcrypt.hash(req.body.Password,12);
+        //console.log(data);
+        const upd=await Student.updateOne({RollNo:req.body.RollNo},{$set:{Password:pass}});
         res.json({
             status:"success"
         })
