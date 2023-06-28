1b5f41ec6f6ba9ebf3ad7554c0d8371d33110332
Yugandharladhe <yugandharladhe74@gmail.com>
2023-06-28 11:34:04 +0530

    some changes

--- tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-const src = [
-//     { a: "dog", b: 8, c: "male", d: "big", e: 100 },
-//     { a: "dog1", b: 5, c: "female", d: "big", e: 200},
-//     { a: "dog2", b: 18, c: "female", d: "big", e: 350},
-//     { a: "dog2", b: 18, c: "female", d: "big", e: 350},
-//     { a: "dog", b: 3, c: "male", d: "big", e: 100 },
-//     { a: "dog", b: 8, c: "male", d: "big", e: 100 },
-//   ]
